12ca9bc5a5c8655f6ef3d089aa8d6ba3352a8e18
AryanParmar007 <124689058+AryanParmar007@users.noreply.github.com>
2023-10-01 15:44:43 +0530

    Initial commit

---
+++ app/src/main/java/com/aryanparmar/mad_practical6_21012011061/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.aryanparmar.mad_practical6_21012011061
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val play: FloatingActionButton = findViewById(R.id.play_icon);
+        val stop: FloatingActionButton = findViewById(R.id.pause_icon);
+        play.setOnClickListener { playbtn() }
+        stop.setOnClickListener { stopbtn() }
+    }
+    fun playbtn()
+    {
+        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,
+            MyService.PLAYERVALUE).apply { startService(this) }
+    }
+    fun stopbtn()
+    {
+        Intent(applicationContext,MyService::class.java).putExtra(MyService.PLAYERKEY,
+            MyService.PLAYERVALUE).apply { stopService(this) }
+    }
+}
